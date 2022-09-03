fa420e1be393e9306929a2b8524507f6b1633bc9
dongduy956 <dongduy0612@gmail.com>
2022-09-03 13:11:40 +0700

    fix pay online vnpay

--- NONBAOHIEMVIETTIN/NONBAOHIEMVIETTIN/Models/VnPayLibrary.cs
+++ NONBAOHIEMVIETTIN/NONBAOHIEMVIETTIN/Models/VnPayLibrary.cs
@@ -56,21 +56,21 @@ namespace NONBAOHIEMVIETTIN.Models
                 }
             }
             string queryString = data.ToString();
-            
+
             baseUrl += "?" + queryString;
             String signData = queryString;
             if (signData.Length > 0)
             {
 
-                signData= signData.Remove(data.Length - 1, 1);
+                signData = signData.Remove(data.Length - 1, 1);
             }
-            string vnp_SecureHash = Utils.HmacSHA512(vnp_HashSecret , signData);
+            string vnp_SecureHash = Utils.HmacSHA512(vnp_HashSecret, signData);
             baseUrl += "vnp_SecureHash=" + vnp_SecureHash;
-           
+
             return baseUrl;
         }
 
-        
+
 
         #endregion
 
@@ -98,7 +98,7 @@ namespace NONBAOHIEMVIETTIN.Models
             {
                 if (!String.IsNullOrEmpty(kv.Value))
                 {
-                    data.Append(WebUtility.UrlEncode( kv.Key) + "=" + WebUtility.UrlEncode(kv.Value )+ "&");
+                    data.Append(WebUtility.UrlEncode(kv.Key) + "=" + WebUtility.UrlEncode(kv.Value) + "&");
                 }
             }
             //remove last '&'
@@ -114,11 +114,11 @@ namespace NONBAOHIEMVIETTIN.Models
 
     public class Utils
     {
-         
+
 
         public static String HmacSHA512(string key, String inputData)
         {
-            var hash = new StringBuilder(); 
+            var hash = new StringBuilder();
             byte[] keyBytes = Encoding.UTF8.GetBytes(key);
             byte[] inputBytes = Encoding.UTF8.GetBytes(inputData);
             using (var hmac = new HMACSHA512(keyBytes))
@@ -139,7 +139,7 @@ namespace NONBAOHIEMVIETTIN.Models
             {
                 ipAddress = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
 
-                if (string.IsNullOrEmpty(ipAddress) || (ipAddress.ToLower() == "unknown")|| ipAddress.Length>45)
+                if (string.IsNullOrEmpty(ipAddress) || (ipAddress.ToLower() == "unknown") || ipAddress.Length > 45)
                     ipAddress = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
             }
             catch (Exception ex)
